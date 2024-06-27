1ffc2538c1f4a4ccb7f9f442ef46abad095a9f46
Rishabh <70062058+rixhabhftw@users.noreply.github.com>
2024-06-27 21:10:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-document.getElementById('startForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+document.getElementById('userInfoForm').addEventListener('submit', function(e) {
+    e.preventDefault();
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     localStorage.setItem('name', name);

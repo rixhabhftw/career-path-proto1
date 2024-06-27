4378cc3aa10d492bb2cd50574848e344ad161252
Rishabh <70062058+rixhabhftw@users.noreply.github.com>
2024-06-27 21:03:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,57 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('surveyForm').addEventListener('submit', function(event) {
-        event.preventDefault();
-
-        const formData = new FormData(event.target);
-        const name = formData.get('name');
-        const email = formData.get('email');
-        const totalQuestions = 54;
-        const scores = {
-            linguistic: 0,
-            logicalMathematical: 0,
-            musical: 0,
-            spatial: 0,
-            bodilyKinesthetic: 0,
-            interpersonal: 0,
-            intrapersonal: 0,
-            naturalist: 0
-        };
-
-        formData.forEach((value, key) => {
-            if (key.startsWith('q')) {
-                const [_, type] = key.split('_');
-                scores[type] += parseInt(value);
-            }
-        });
-
-        const data = {
-            name: name,
-            email: email,
-            linguistic: ((scores.linguistic / (totalQuestions / 8 * 5)) * 100).toFixed(2),
-            logicalMathematical: ((scores.logicalMathematical / (totalQuestions / 8 * 5)) * 100).toFixed(2),
-            musical: ((scores.musical / (totalQuestions / 8 * 5)) * 100).toFixed(2),
-            spatial: ((scores.spatial / (totalQuestions / 8 * 5)) * 100).toFixed(2),
-            bodilyKinesthetic: ((scores.bodilyKinesthetic / (totalQuestions / 8 * 5)) * 100).toFixed(2),
-            interpersonal: ((scores.interpersonal / (totalQuestions / 8 * 5)) * 100).toFixed(2),
-            intrapersonal: ((scores.intrapersonal / (totalQuestions / 8 * 5)) * 100).toFixed(2),
-            naturalist: ((scores.naturalist / (totalQuestions / 8 * 5)) * 100).toFixed(2)
-        };
-
-        fetch('https://script.google.com/macros/s/AKfycbz8bKrhVIToCc-HvPuh7PMjJe0mZRX_gs0AbVhpAMk5EbXbeSwqlFkdRc2OMZaqmZ6yxA/exec', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(data)
-        })
-        .then(response => response.json())
-        .then(result => {
-            if (result.result === 'success') {
-                alert('Survey submitted successfully!');
-            } else {
-                alert('Error submitting survey.');
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-    });
+document.getElementById('startForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    localStorage.setItem('name', name);
+    localStorage.setItem('email', email);
+    window.location.href = 'survey.html';
 });
